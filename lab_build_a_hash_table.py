68471e75cf7775fc912c116a221134d39a5ab899
Leonardo Cerqueira <leonevescer@gmail.com>
2026-04-24 19:07:19 -0300

    Add a docstring

--- lab_build_a_hash_table.py
+++ lab_build_a_hash_table.py
@@ -1,3 +1,14 @@
+'''
+Build a hash table from scratch. A hash table is a data structure that stores key-value pairs.
+A hash table works by taking the key as an input and then hashing this key according
+to a specific hashing function.
+
+For the purpose of this lab, the hashing function will be simple: it will sum
+the Unicode values of each character in the key. The hash value will then be used
+as the actual key to store the associated value. The same hash value would also be used
+to retrieve and delete the value associated with the key.
+'''
+
 class HashTable:
     def __init__(self):
         self.collection={}
